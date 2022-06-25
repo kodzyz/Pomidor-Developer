c0aceb97426c270e6e6808fa4e2d83343bc3e298
ko_dzy <kodzyzdok@gmail.com>
2022-06-25 17:48:40 +0300

    PostgreSQL

--- books/books/settings.py
+++ books/books/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'books.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookdb',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '463580',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
