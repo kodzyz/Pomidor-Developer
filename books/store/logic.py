9eb2fd75cc3ad31631acff596d33932b18d93e46
ko_dzy <kodzyzdok@gmail.com>
2022-07-02 21:43:33 +0300

    Бекенд на Django, Урок 2: Unit Tests (SeniorPomidorDeveloper)

---
+++ books/store/logic.py
@@ -0,0 +1,16 @@
+def operations(a, b, c):
+    if c == '+':
+        return a + b
+    if c == '-':
+        return a - b
+    if c == '*':
+        return a * b
+
+# Python Console
+# from books.store.logic import operations
+# operations(2,2,'+')
+# Out[10]: 4
+# operations(2,2,'-')
+# Out[11]: 0
+# operations(2,2,'*')
+# Out[12]: 4
