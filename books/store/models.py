1669d823ba66a45b1d544fcd46fce2a6a33c76fd
ko_dzy <kodzyzdok@gmail.com>
2022-07-06 21:41:13 +0300

    init

--- books/store/models.py
+++ books/store/models.py
@@ -7,27 +7,5 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255, default='author')
 
-    # определяем __str__  через F-strings в python
     def __str__(self):
         return f'Id {self.id}: {self.name}'
-
-
-
-# ./manage.py shell
-# In [1]: from store.models import Book
-#
-# In [2]: Book.objects.all()
-# Out[2]: <QuerySet [<Book: Book object (1)>, <Book: Book object (2)>]>
-#
-# In [3]: Book.objects.all()[0].price
-# Out[3]: Decimal('1000.00')
-#
-# In [4]: Book.objects.all()[1].price
-# Out[4]: Decimal('1000.00')
-#
-# In [5]: Book.objects.create(name='Perfume', price='500.00')
-
-
-# http://127.0.0.1:8000/book/?price=500 -> [{"id":3,"name":"Perfume","price":"500.00"}]
-
-
--- books/store/views.py
+++ books/store/views.py
@@ -12,14 +12,11 @@ from .serializers import BookSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    # настройка фильтрации через API , filter_fields, DjangoFilterBackend
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     permission_classes = [
-        IsAuthenticated]  # https://python-social-auth.readthedocs.io/en/latest/configuration/django.html
+        IsAuthenticated]
     filterset_fields = ['price']  # http://127.0.0.1:8000/book/?price=1000
-    # настройка поиска через search_fields в API View , SearchFilter
     search_fields = ['name', 'author_name']  # http://127.0.0.1:8000/book/?search=Hemingway
-    # Ordering в Django REST Framework, OrderingFilter, ordering_fields
     ordering_fields = ['price', 'author_name']  # http://127.0.0.1:8000/book/?ordering=price
 
 
